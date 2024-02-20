354ccbe53064f28ace1b3b28ffc66430c750e2d1
weakkitten <146001313+weakkitten@users.noreply.github.com>
2024-02-20 21:18:36 +0400

    Add files via upload

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -5,6 +5,7 @@ import lombok.NonNull;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PastOrPresent;
 import java.time.LocalDate;
 
 @Data
@@ -17,6 +18,6 @@ public class User {
     protected String login;
     @NonNull
     protected String name;
-    @NonNull
+    @NonNull @PastOrPresent
     protected LocalDate birthday;
 }
